4d85c9cd30e792769962371a307b1e92ea7f572e
Jacob Marthaller <marthaller.jr@gmail.com>
2021-03-29 06:32:35 -0700

    working solution

---
+++ index.js
@@ -0,0 +1,9 @@
+for (let x = 1; x <= 101; x++) {
+    let output = "";
+    if (x % 3 == 0) {
+        output += "Fizz";
+    } else if (x % 5 == 0) {
+      output += "Buzz";
+    }
+    console.log(output || x);
+  }
